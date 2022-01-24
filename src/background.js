85bf658bdee1c9490be0cf25c115bcfbf3642e33
Bruno FS <bruno.serbena@gmail.com>
2022-01-24 18:27:03 -0300

    Started background.js V3 conversion, Battery API unsupported
    Signed-off-by: Bruno FS <bruno.serbena@gmail.com>

--- src/background.js
+++ src/background.js
@@ -1,197 +1,197 @@
-function init(chrome, navigator) {
-  /**
-   * Time to text conversions
-   */
 
-  window.formatTime = function (seconds) {
-    var minutes = Math.floor(seconds / 60),
-        hours = Math.floor(minutes / 60),
-        days = Math.floor(hours / 24),
+/**
+ * Time to text conversions
+ */
 
-        moddedMinutes = minutes % 60,
-        moddedHours = hours % 24,
+var formatTime = function (seconds) {
+  var minutes = Math.floor(seconds / 60),
+      hours = Math.floor(minutes / 60),
+      days = Math.floor(hours / 24),
 
-        minutesText = (moddedMinutes == 1) ? "1 minute" : moddedMinutes + " minutes",
-        hoursText = (moddedHours == 1) ? "1 hour" : moddedHours + " hours",
-        daysText = (days == 1) ? "1 day" : days + " days";
+      moddedMinutes = minutes % 60,
+      moddedHours = hours % 24,
 
-    if (days >= 1) {
-      return (moddedHours === 0) ? daysText : daysText + ", " + hoursText;
-    }
-    else if (hours >= 1) {
-      return (moddedMinutes === 0) ? hoursText : hoursText + ", " + minutesText;
-    }
-    else {
-      return minutesText;
-    }
-  };
+      minutesText = (moddedMinutes == 1) ? "1 minute" : moddedMinutes + " minutes",
+      hoursText = (moddedHours == 1) ? "1 hour" : moddedHours + " hours",
+      daysText = (days == 1) ? "1 day" : days + " days";
 
-  window.unformatTime = function (count, unit) {
-    switch (unit) {
-      case "seconds":
-        return count;
-      case "minutes":
-        return unformatTime(count * 60, "seconds");
-      case "hours":
-        return unformatTime(count * 60, "minutes");
-      case "days":
-        return unformatTime(count * 24, "hours");
-    }
-  };
+  if (days >= 1) {
+    return (moddedHours === 0) ? daysText : daysText + ", " + hoursText;
+  }
+  else if (hours >= 1) {
+    return (moddedMinutes === 0) ? hoursText : hoursText + ", " + minutesText;
+  }
+  else {
+    return minutesText;
+  }
+};
 
-
-  /*
-   * Triggers
-   */
-  window.Trigger = function(_percent, _timeAmount, _timeUnit) {
-    this.percent = _percent || null;
-    if (_timeAmount && _timeUnit) {
-      this.timeAmount = _timeAmount;
-      this.timeUnit = _timeUnit;
-    } else {
-      this.timeAmount = null;
-      this.timeUnit = null;
-    }
-
-    return this;
-  };
-
-  Trigger.prototype.check = function(battery) {
-    if (this.percent !== null) {
-      if(this.percent > Math.ceil(battery.level * 100)) {
-        return true;
-      }
-    }
-
-    if (this.time() !== null) {
-      if (this.time() > battery.dischargingTime) {
-        return true;
-      }
-    }
-
-    /* otherwise.. */
-    return false;
-  };
-
-  Trigger.prototype.time = function() {
-    if (this.timeAmount !== null && this.timeUnit !== null) {
-      return unformatTime(this.timeAmount, this.timeUnit);
-    } else {
-      return null;
-    }
-  };
-
-  Trigger.fromJsonObject = function(obj) {
-    return new Trigger(obj.percent, obj.timeAmount, obj.timeUnit);
-  };
+var unformatTime = function (count, unit) {
+  switch (unit) {
+    case "seconds":
+      return count;
+    case "minutes":
+      return unformatTime(count * 60, "seconds");
+    case "hours":
+      return unformatTime(count * 60, "minutes");
+    case "days":
+      return unformatTime(count * 24, "hours");
+  }
+};
 
 
-  /**
-   * Warnings
-   */
-  window.Warning = function(options) {
-    options = options || {};
+/*
+  * Triggers
+  */
+var Trigger = function(_percent, _timeAmount, _timeUnit) {
+  this.percent = _percent || null;
+  if (_timeAmount && _timeUnit) {
+    this.timeAmount = _timeAmount;
+    this.timeUnit = _timeUnit;
+  } else {
+    this.timeAmount = null;
+    this.timeUnit = null;
+  }
 
-    this.enabled = ('enabled' in options) ? options.enabled : true;
-    this.trigger = ('trigger' in options) ? options.trigger : new Trigger(5);
+  return this;
+};
 
-    this.shown = false;
-  };
-
-  Warning.prototype.checkBattery = function(battery) {
-
-    var isTriggered = this.trigger.check(battery);
-
-    if (this.enabled) {
-    if (!battery.charging) {
-    if (!this.shown && isTriggered) {
-      this.showNotification(battery);
-    }}}
-
-    this.shown = isTriggered;
-  };
-
-  Warning.prototype.showNotification = function(battery) {
-    var percentage = Math.floor(battery.level * 100);
-    var myAudio = new Audio(chrome.runtime.getURL("assets/alert-bells-echo.wav"));
-    myAudio.play();
-    chrome.notifications.create(
-      "battery-notifier",
-      {
-        type: "basic",
-        title: percentage + "% power left",
-        message: "",
-        contextMessage: formatTime(battery.dischargingTime) + " remaining",
-        isClickable: false,
-        iconUrl: "assets/icon_128.png"
-      },
-      function () {}
-    );
-  };
-
-  chrome.notifications.onClicked.addListener(function (notificationId) {
-    chrome.notifications.clear(notificationId, function () {});
-  });
-
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.type == "setOptions") {
-      setOptions(request.options);
-    } else if (request.type == "getOptions") {
-      sendResponse(warnings);
-    }
-    return true;
-  });
-
-  Warning.fromJsonObject = function(obj) {
-    obj.trigger = Trigger.fromJsonObject(obj.trigger);
-    return new Warning(obj);
-  };
-
-
-  var warnings = [
-    new Warning({})
-  ];
-
-  var setOptions = function(json) {
-    warnings = json.map(function (obj) {
-      return Warning.fromJsonObject(obj);
-    });
-    console.log("Options set.");
-  };
-
-  function loadFromStorage() {
-    var storeKey = "warnings";
-    try {
-      chrome.storage.local.get(storeKey, function(results) {
-        if (results[storeKey]) {
-          var json = results[storeKey];
-          setOptions(json);
-          console.log(warnings.length + " settings loaded from local storage.");
-        }  else {
-          console.log("No settings found.");
-        }
-      });
-    }
-    catch (err) {
-      console.log("Error loading settings from local storage:");
-      console.log(err + " " + JSON.stringify(err));
-      return null;
+Trigger.prototype.check = function(battery) {
+  if (this.percent !== null) {
+    if(this.percent > Math.ceil(battery.level * 100)) {
+      return true;
     }
   }
 
+  if (this.time() !== null) {
+    if (this.time() > battery.dischargingTime) {
+      return true;
+    }
+  }
 
-  /* See https://developer.mozilla.org/en-US/docs/Web/API/Battery_Status_API */
-  navigator.getBattery().then(function(battery){
+  /* otherwise.. */
+  return false;
+};
 
-    loadFromStorage();
+Trigger.prototype.time = function() {
+  if (this.timeAmount !== null && this.timeUnit !== null) {
+    return unformatTime(this.timeAmount, this.timeUnit);
+  } else {
+    return null;
+  }
+};
 
-    battery.addEventListener('levelchange', function() {
-      warnings.forEach(function (warning) {
-        warning.checkBattery(battery);
-      });
-    });
+Trigger.fromJsonObject = function(obj) {
+  return new Trigger(obj.percent, obj.timeAmount, obj.timeUnit);
+};
 
-  }, function() {
-    console.log("no battery found");
+
+/**
+ * Warnings
+ */
+var Warning = function(options) {
+  options = options || {};
+
+  this.enabled = ('enabled' in options) ? options.enabled : true;
+  this.trigger = ('trigger' in options) ? options.trigger : new Trigger(5);
+
+  this.shown = false;
+};
+
+Warning.prototype.checkBattery = function(battery) {
+
+  var isTriggered = this.trigger.check(battery);
+
+  if (this.enabled) {
+  if (!battery.charging) {
+  if (!this.shown && isTriggered) {
+    this.showNotification(battery);
+  }}}
+
+  this.shown = isTriggered;
+};
+
+Warning.prototype.showNotification = function(battery) {
+  var percentage = Math.floor(battery.level * 100);
+  var myAudio = new Audio(chrome.runtime.getURL("assets/alert-bells-echo.wav"));
+  myAudio.play();
+  chrome.notifications.create(
+    "battery-notifier",
+    {
+      type: "basic",
+      title: percentage + "% power left",
+      message: "",
+      contextMessage: formatTime(battery.dischargingTime) + " remaining",
+      isClickable: false,
+      iconUrl: "assets/icon_128.png"
+    },
+    function () {}
+  );
+};
+
+chrome.notifications.onClicked.addListener(function (notificationId) {
+  chrome.notifications.clear(notificationId, function () {});
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.type == "setOptions") {
+    setOptions(request.options);
+  } else if (request.type == "getOptions") {
+    sendResponse(warnings);
+  }
+  return true;
+});
+
+Warning.fromJsonObject = function(obj) {
+  obj.trigger = Trigger.fromJsonObject(obj.trigger);
+  return new Warning(obj);
+};
+
+
+var warnings = [
+  new Warning({})
+];
+
+var setOptions = function(json) {
+  warnings = json.map(function (obj) {
+    return Warning.fromJsonObject(obj);
   });
+  console.log("Options set.");
+};
+
+function loadFromStorage() {
+  var storeKey = "warnings";
+  try {
+    chrome.storage.local.get(storeKey, function(results) {
+      if (results[storeKey]) {
+        var json = results[storeKey];
+        setOptions(json);
+        console.log(warnings.length + " settings loaded from local storage.");
+      }  else {
+        console.log("No settings found.");
+      }
+    });
+  }
+  catch (err) {
+    console.log("Error loading settings from local storage:");
+    console.log(err + " " + JSON.stringify(err));
+    return null;
+  }
 }
+
+
+/* See https://developer.mozilla.org/en-US/docs/Web/API/Battery_Status_API */
+navigator.getBattery().then(function(battery){
+
+  loadFromStorage();
+
+  battery.addEventListener('levelchange', function() {
+    warnings.forEach(function (warning) {
+      warning.checkBattery(battery);
+    });
+  });
+
+}, function() {
+  console.log("no battery found");
+});
+
